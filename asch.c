a57913b288aba9fab908551bcc92f4705ccd6bdf
Jairo <jairo.moutinho@edu.ufes.br>
2020-11-27 16:38:23 -0300

    :construction: processo gerenciador chamando execvp corretamente, mas o código está mais feio que eu

--- asch.c
+++ asch.c
@@ -3,8 +3,12 @@
 #include <stddef.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
+
 #include "asch.h"
+#include "linkedList.h"
 
 void trata_sinal(int sinal)
 {
@@ -19,7 +23,7 @@ void trata_sinal(int sinal)
         break;
     case SIGTSTP:
         caractere_chamada = 'Z';
-        break;  
+        break;
     default:
         printf("%d", sinal);
         break;
@@ -28,64 +32,105 @@ void trata_sinal(int sinal)
     printf("\nNão adianta me enviar o sinal por Ctrl-%c. Estou vacinado!!\n", caractere_chamada);
 }
 
-
 int muda_diretorio(char *path)
 {
-
+    if (path == NULL)
+        printf("CD sem argumentos, nada será feito");
+    return -1;
     int result = chdir(path);
     return result;
 }
 
-
-int trata_comando()
+int trata_comando_foreground(char *comando)
 {
-    char comando[1000];
-    fgets(comando, 1000, stdin);
-    char* tokenized_command[8];
-    int cd_flag = 0 ;
-    char* token;
+    char *command_token;
     const char delimiter[] = " ";
-    
-    token = strtok(comando, delimiter);
-    int count = 0;
-    while(token != NULL)
+    command_token = strtok(comando, delimiter);
+
+    if (strcmp(command_token, "cd") == 0)
     {
-        tokenized_command[count] = malloc(sizeof(char) * strlen(token));
-        strcpy(tokenized_command[count], token);
-        count++;
-        token = strtok(NULL, delimiter);
+        command_token = strtok(NULL, delimiter);
+        muda_diretorio(command_token);
+        return 0;
     }
 
-    int n_tokens = count; // reference sugar
+    else if (strcmp(command_token, "exit") == 0)
+        exit(1);
 
-    for(int i = 0; i < n_tokens; i++)
+    else
     {
-        printf("%s ", tokenized_command[i]);
-
-    }
-    
-    int flag;
-    if(strcmp(tokenized_command[0], "cd") == 0)
-    {
-        flag = chdir(tokenized_command[1]);
-        printf("%s.", tokenized_command[1]);
-        printf("INFO ---- chdir result: %d\n", flag);
-        system("ls");
+        return 1;
     }
 }
 
+int trata_comando_background(char *comando)
+{
+    char *argv[8];
+    int cd_flag = 0;
+    char *command_token;
+    char *argv_token;
+    const char delimiter[] = "<3";
+    // List* parsed_commands_list;
+    // List* parsed_command;
+    // if(newList(parsed_commands_list, sizeof(List*), NULL))
+    // if(newList(parsed_command, sizeof(char*), NULL))
+    char *end_str;
+    command_token = strtok_r(comando, delimiter, &end_str);
+    while (command_token != NULL)
+    {   
 
+        int argv_len = 0;
+        char *end_token;
+        argv_token = strtok_r(command_token, " ", &end_token);    
+        while(argv_token != NULL)
+        {
+            
+            argv[argv_len] = malloc(sizeof(char) * strlen(argv_token));
+            strcpy(argv[argv_len], argv_token);
+            argv_len++;
+            argv_token = strtok_r(NULL, " ", &end_token);
+        }
+        
+        char nome_arquivo[strlen(argv[0]) + 3];
+        strcpy(nome_arquivo, "./"); strcat(nome_arquivo, argv[0]);
+        argv[argv_len] = NULL;
+        if(execvp(nome_arquivo, argv) < 0) perror("erro");
+        command_token = strtok_r(NULL, delimiter, &end_str);
+    }
+
+}
 
 int main()
-{   
+{
+    /// Define tratamento de sinais ///
     struct sigaction tratador_sinais = {.sa_handler = trata_sinal};
-    sigemptyset(&tratador_sinais.sa_mask);  
+    sigemptyset(&tratador_sinais.sa_mask);
     sigaddset(&tratador_sinais.sa_mask, SIGINT);
     sigaddset(&tratador_sinais.sa_mask, SIGQUIT);
     sigaddset(&tratador_sinais.sa_mask, SIGTSTP);
     sigaction(SIGINT, &tratador_sinais, NULL);
     sigaction(SIGQUIT, &tratador_sinais, NULL);
     sigaction(SIGTSTP, &tratador_sinais, NULL);
+    pid_t pid_gerenciador;
+    ////
     printf(">>>>  ");
-    trata_comando(); //forkar aqui
+    char comando[1000];
+    char comando_cpy[1000];
+    fgets(comando, 1000, stdin);
+    strcpy(comando_cpy, comando);
+    int background_flag = trata_comando_foreground(comando);
+
+    if (background_flag)
+        pid_gerenciador = fork();
+    
+    if (pid_gerenciador == 0)
+    {   
+        setsid();
+        trata_comando_background(comando_cpy);
+    }
+
+    if (pid_gerenciador > 0)
+    {
+        waitpid(pid_gerenciador, NULL, WNOHANG);
+    }
 }
